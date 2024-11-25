4c403aa45546265478fca443a805ca894669bb35
Talha Fiaz <talhafiaz09@gmail.com>
2024-11-25 06:33:56 +0000

    Added vercel build script

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,3 +1,3 @@
-export const apiKey =
-    "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";
+export const apiKey = process.env.OPENAI;
+
 export const DATA_STORAGE_KEY = "ai_powered_data";
